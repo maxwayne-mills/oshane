d6464f7ac9a640118d3d477b2871b71ff40d0bbe
Clarence Mills <clarence.mills@gmail.com>
2018-11-18 12:36:49 -0500

    New ansible options setup.yml and requirements.yml for future use

---
+++ digital_cean/firewall.tf
@@ -0,0 +1,25 @@
+resource "digitalocean_firewall" "srv-0" {
+  name        = "Access-to-Server"
+  droplet_ids = ["${digitalocean_droplet.srv-0.id}"]
+
+  inbound_rule = [
+    {
+      protocol         = "tcp"
+      port_range       = "22"
+      source_addresses = ["0.0.0.0/0", "::/0"]
+    },
+  ]
+
+  outbound_rule = [
+    {
+      protocol              = "tcp"
+      port_range            = "1-65535"
+      destination_addresses = ["0.0.0.0/0", "::/0"]
+    },
+    {
+      protocol              = "udp"
+      port_range            = "1-65535"
+      destination_addresses = ["0.0.0.0/0", "::/0"]
+    },
+  ]
+}
--- digital_cean/srv.tf
+++ digital_cean/srv.tf
@@ -1,4 +1,4 @@
-resource "digitalocean_droplet" "srv1" {
+resource "digitalocean_droplet" "srv-0" {
   image     = "${var.droplet_image}"
   name      = "${var.srv_name}"
   region    = "${var.region}"
@@ -8,25 +8,11 @@ resource "digitalocean_droplet" "srv1" {
 
   # Create ansible configuration file
   provisioner "local-exec" {
-    command = "sleep 20 && echo \"[srv1]\n${digitalocean_droplet.srv1.ipv4_address} ansible_connection=ssh ansible_ssh_user=deployuser\" > inventory"
-  }
-
-  # Run shell script to install apache and deploy domain(s)
-  provisioner "local-exec" {
-    command = "sleep 120 && shell_scripts/execute_roles.sh"
+    command = "sleep 20 && echo \"[srv-0]\n${digitalocean_droplet.srv-0.ipv4_address} ansible_connection=ssh ansible_ssh_user=deployuser\" > inventory"
   }
 }
 
-resource "digitalocean_floating_ip" "srv1" {
-  droplet_id = "${digitalocean_droplet.srv1.id}"
+resource "digitalocean_floating_ip" "srv-0" {
+  droplet_id = "${digitalocean_droplet.srv-0.id}"
   region     = "${var.region}"
 }
-
-/*
-# Create A record for domain.
-resource "digitalocean_domain" "site" {
-  name       = "www.fixyourip.com"
-  ip_address = "${digitalocean_droplet.srv1.ipv4_address}"
-}
-*/
-
